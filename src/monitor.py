f8c6045a1cd18e1d02cb918e088045a96923cea2
bodyheath <bodyheath@gmail.com>
2026-06-25 17:34:41 +1200

    Auto-commit: 2026-06-25 17:34:41

--- src/monitor.py
+++ src/monitor.py
@@ -2235,7 +2235,7 @@ def _check_pending_trades(prices: dict, log_fn=None) -> list:
                     else actual_entry + orig_stop_pips * ps_size
                 )
                 df.loc[idx, "stop_loss"] = round(new_stop, 5)
-                for t_col in ("target", "t2_price", "t3_price"):
+                for t_col in ("target", "t1_price", "t2_price", "t3_price"):
                     try:
                         orig_t = float(t.get(t_col) or 0)
                         if orig_t > 0:
